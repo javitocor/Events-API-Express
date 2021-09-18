586510413bb415afefa6be2828e9b7c5572b4fe0
javitocor <javitocor@gmail.com>
2021-09-18 14:41:45 +0700

    add more protected routes to users

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,8 @@ const passport = require('passport');
 
 var user_controller = require('../controllers/userController');
 
-router.get('/', user_controller.user_list);
-router.get('/:id', user_controller.user_detail);
+router.get('/', passport.authenticate('bearer', { session: false }), user_controller.user_list);
+router.get('/:id', passport.authenticate('bearer', { session: false }), user_controller.user_detail);
 router.put('/:id', passport.authenticate('bearer', { session: false }), user_controller.user_update);
 router.delete('/:id', passport.authenticate('bearer', { session: false }), user_controller.user_delete);
 
