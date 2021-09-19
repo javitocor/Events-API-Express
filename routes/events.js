04ebb57a6fb1b5daaf4469d1c26eec6d18ecd5c1
javitocor <javitocor@gmail.com>
2021-09-19 11:50:59 +0700

    add secure routes to events

--- routes/events.js
+++ routes/events.js
@@ -7,7 +7,7 @@ const permissions = require('../permissionsMiddleware/permissions');
 var event_controller = require('../controllers/eventController');
 
 router.get('/', event_controller.event_list);
-router.get('/all', event_controller.event_list_all);
+router.get('/all', passport.authenticate('jwt', { session: false }), permissions.checkIsSuperadmin, event_controller.event_list_all);
 router.get('/:id', event_controller.event_detail);
 router.post('/', passport.authenticate('jwt', { session: false }), permissions.checkIsAdminBasic, event_controller.event_create);
 router.put('/:id', passport.authenticate('jwt', { session: false }), permissions.checkIsSuperadmin, event_controller.event_update);
--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ var user_controller = require('../controllers/userController');
 
 
 router.get('/', passport.authenticate('jwt', { session: false }), permissions.checkIsAdminBasic, user_controller.user_list);
-router.get('/:id', passport.authenticate('jwt', { session: false }), permissions.checkIsSuperadmin, user_controller.user_detail);
+router.get('/:id', passport.authenticate('jwt', { session: false }), permissions.checkIsAdminManager, user_controller.user_detail);
 router.put('/:id', passport.authenticate('jwt', { session: false }), permissions.checkIsSuperadmin, user_controller.user_update);
 router.delete('/:id', passport.authenticate('jwt', { session: false }), permissions.checkIsSuperadmin, user_controller.user_delete);
 
